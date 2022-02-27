56baa8f3a03335be0432c58e055fe121a4541d1b
Saleem Maini <33354178+SaleemMaini@users.noreply.github.com>
2022-02-27 23:30:01 +0200

    Remove todo feature

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,15 @@ function App() {
     const newTodo = new Todo(todoText);
     setTodos([...todos, newTodo]);
   };
-
+  const removeTodoHandler = (todoId: number) => {
+    setTodos((prevTodos)=>{
+      return prevTodos.filter(todo => todo.id !== todoId)
+    })
+  };
   return (
     <div className="App">
       <NewTodo onAddTodo={addTodoHandler} />
-      <Todos items={todos} />
+      <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
     </div>
   );
 }
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import Todo from '../models/todo'
 import classes from './TodoItem.module.css';
-const TodoItem: React.FC<{ text: string }> = (props) => {
+const TodoItem: React.FC<{ text: string , onRemoveTodo : () => void}> = (props) => {
   return (
-    <li className={classes.item}>{props.text}</li>
+    <li className={classes.item} onClick={props.onRemoveTodo}>{props.text}</li>
   )
 }
 
